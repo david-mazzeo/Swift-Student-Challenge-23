be6aeec4dc03d8c98fdf10b1238bcf0b00554d4a
David <lou.mazzeo.developer21@gmail.com>
2023-04-07 11:41:09 +1000

    Remade motion engine with SpriteKit.

--- SpaceFlightController.swift
+++ SpaceFlightController.swift
@@ -11,57 +11,72 @@ import CoreMotion
 
 class SpaceFlightController: UIViewController {
     
-    @IBOutlet weak var rocketView: UIImageView!
-    let motionEngine = CMMotionManager()
-    var previousRotation: Double = 0
-    
-    let rocketImages = [UIImage(named: "Rocket 1")!,
-                        UIImage(named: "Rocket 2")!,
-                        UIImage(named: "Rocket 3")!,
-                        UIImage(named: "Rocket 3A")!,
-                        UIImage(named: "Rocket 4")!,
-                        UIImage(named: "Rocket 5")!,
-                        UIImage(named: "Rocket 6")!]
+    @IBOutlet weak var spriteKitView: SKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        spriteKitView.presentScene(FlightScene(size: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)))
         
-        rocketView.layer.magnificationFilter = .nearest
-        rocketView.animationImages = rocketImages
-        rocketView.animationDuration = 1
-        rocketView.animationRepeatCount = 0
-        rocketView.startAnimating()
+    }
+    
+}
+
+class FlightScene: SKScene {
+    
+    let motionEngine = CMMotionManager()
+    let rocketImages = [SKTexture(image: UIImage(named: "Rocket 1")!),
+                        SKTexture(image: UIImage(named: "Rocket 2")!),
+                        SKTexture(image: UIImage(named: "Rocket 3")!),
+                        SKTexture(image: UIImage(named: "Rocket 4")!),
+                        SKTexture(image: UIImage(named: "Rocket 3A")!),
+                        SKTexture(image: UIImage(named: "Rocket 5")!),
+                        SKTexture(image: UIImage(named: "Rocket 6")!)]
+    
+    let protagonist = Rocket(imageNamed: "Rocket 1.png")
+    
+    override func didMove(to view: SKView) {
+        
+        for image in rocketImages {
+            image.filteringMode = .nearest
+        }
         
         motionEngine.accelerometerUpdateInterval = 1/60
         motionEngine.gyroUpdateInterval = 1/60
         
-        motionEngine.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { [self] (data, error) -> Void in
+        motionEngine.startAccelerometerUpdates()
+        run(SKAction.run { [self] in
             
-            let xAxis = data?.acceleration.x ?? 0.0
-            if isAboveThreshold(nextRotation: xAxis) {
-                
-                let xAxisAdjusted = (xAxis + 1) / 2
-                let screenWidth = UIScreen.main.bounds.width
-                let imageWidth = rocketView.bounds.width / 2
-                
-                rocketView.transform = CGAffineTransform(translationX: CGFloat(xAxisAdjusted * screenWidth) - imageWidth, y: 0)
-            }
+            protagonist.size = CGSize(width: 72, height: 120)
+            protagonist.zPosition = 1
+            protagonist.position = CGPoint(x: 50, y: 90)
+
+            let rocketAnimation = SKAction.repeatForever(SKAction.animate(with: rocketImages, timePerFrame: 0.1))
+            protagonist.run(rocketAnimation)
             
-            print(xAxis as Any)
+            self.addChild(protagonist)
             
         })
         
     }
     
-    func isAboveThreshold(nextRotation: Double) -> Bool {
-        let ratio = previousRotation - nextRotation
-        
-        if ratio >= 0.1 || ratio <= -0.1 {
-            previousRotation = nextRotation
-            return true
-        } else {
-            return false
-        }
+    override func update(_ currentTime: TimeInterval) {
+        processUserMotion(forUpdate: currentTime)
     }
+    
+    func processUserMotion(forUpdate currentTime: CFTimeInterval) {
+        let xAxis = motionEngine.accelerometerData?.acceleration.x ?? 0.0
+        let xAxisAdjusted = (xAxis + 1) / 2
+        let screenWidth = UIScreen.main.bounds.width
+
+        protagonist.position = CGPoint(x: CGFloat(xAxisAdjusted * screenWidth), y: 90)
+        
+        print(xAxis as Any)
+    }
+    
+    
+}
+
+class Rocket: SKSpriteNode {
     
 }
